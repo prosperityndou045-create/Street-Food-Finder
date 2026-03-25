cc1c0f4fb26996d955057cfa2a3b62aabee1dc99
Prosperity <prosperityndou045@gmail.com>
2026-03-25 15:38:49 +0200

    Initial Commit

---
+++ Street Food Finder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Street Food Finder
+//
+//  Created by Prosperity on 25/3/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Street Food Finder/Street_Food_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Street_Food_FinderApp.swift
+//  Street Food Finder
+//
+//  Created by Prosperity on 25/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct Street_Food_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
