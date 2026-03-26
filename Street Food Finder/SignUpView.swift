a549ced2e66c4e512853f33c81dce15fb2a81ca7
Prosperity <prosperityndou045@gmail.com>
2026-03-26 09:39:03 +0200

    I created WelcomeView

---
+++ Street Food Finder/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  Street Food Finder
+//
+//  Created by Prosperity on 25/3/2026.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpView()
+}
--- Street Food Finder/Street_Food_FinderApp.swift
+++ Street Food Finder/Street_Food_FinderApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Street_Food_FinderApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            WelcomeView()
         }
     }
 }
